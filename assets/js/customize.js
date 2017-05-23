ad5032a989674a9d42523576375e79d25f04bfd8
Cain <i@cainiu.de>
2017-05-23 21:58:02 +0800

    Update customize.js

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -19,12 +19,3 @@ $('.loading-trigger').on('click', function () {
 });
 // masthead background
 $('.ui.inverted.masthead.segment').addClass('bg' + Math.ceil(Math.random() * 14)).removeClass('zoomed');
-
-//baidu 
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?72e6641dc820f9f3787c7d701f0635cf";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
